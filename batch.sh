1e0cc0158a70abd4cd6de8f0d33cf677550934b3
wzh4464 <32484940+wzh4464@users.noreply.github.com>
2023-12-02 23:01:15 +0800

    without meta

--- batch.sh
+++ batch.sh
@@ -3,7 +3,7 @@
  # Created Date: Saturday December 2nd 2023
  # Author: Zihan
  # -----
- # Last Modified: Saturday, 2nd December 2023 7:07:51 am
+ # Last Modified: Saturday, 2nd December 2023 10:04:24 pm
  # Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
  # -----
  # HISTORY:
@@ -23,11 +23,11 @@ export RANK=0
 export LOCAL_RANK=0
 
 # 确保 output_dir 存在
-output_dir="result/cifar10_22k_2"
+output_dir="result/cifar10_22k_wo_meta"
 mkdir -p $output_dir
 
 # 运行分布式 Python 脚本
-python -m torch.distributed.launch \
+~/miniconda3/envs/dam_vp/bin/python -m torch.distributed.launch \
     --nproc_per_node=3 \
     --nnodes=1 \
     --node_rank=0 \
@@ -36,6 +36,7 @@ python -m torch.distributed.launch \
     task_adapting/main.py \
     --output_dir $output_dir \
     --batch_size 96 \
+    --wo_da \
     --base_dir ~/dataset \
     --pretrained_model vit-b-22k \
     --adapt_method prompt_w_head \
@@ -43,7 +44,7 @@ python -m torch.distributed.launch \
     --epochs 50 \
     --lr 0.5 \
     --weight_decay 1e-4 \
-    --checkpoint_dir checkpoints/vit-b-22k-w-head.pth \
+    --checkpoint_dir checkpoints/vit-b-22k-wo-head.pth \
     --dataset cifar10 \
     --num_gpus 3 \
     --distributed 2> $output_dir/error.txt
