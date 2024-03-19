8d8e29d627f0245ac27b12cd591152ef271e3fd5
HanceWu_HPC <32484940+wzh4464@users.noreply.github.com>
2024-03-19 23:42:46 +0800

    origin_cifar100_22k_batchsize8-64-256

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -3,11 +3,9 @@
 #SBATCH --nodes=1                      # 使用的节点数
 #SBATCH --ntasks-per-node=1            # 每个节点的任务数
 #SBATCH --gres=gpu:8                   # 每个节点使用的 GPU 数量
-#SBATCH --output=svhn-vit-distributed_output_%j.txt # 标准输出和错误的文件名
 #SBATCH --time=12:00:00                # 预计作业运行时间
 #SBATCH --mem=102400                      # 内存要求
-#SBATCH --partition=gpu-v100s-test           # 选择 partition
-
+#SBATCH --partition=gpu_v100s           # 选择 partition
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export MASTER_ADDR=localhost
@@ -18,23 +16,22 @@ export NCCL_TIMEOUT=200
 
 # nvidia-smi
 
-/root/miniconda3/envs/lmeraser/bin/python -m torch.distributed.launch \
+/home/zihanwu7/miniconda3/envs/dam-vp/bin/python -m torch.distributed.launch \
     --nproc_per_node=8 \
     --nnodes=1 \
     --node_rank=0 \
     --master_addr=$MASTER_ADDR \
     --master_port=$MASTER_PORT \
     task_adapting/main.py \
-    --output_dir /workspace/result/svhn-vit-distributed_without_head_with_meta \
+    --output_dir result/origin_cifar100_22k_batchsize256\
     --batch_size 256 \
     --base_dir ~/dataset \
     --pretrained_model vit-b-22k \
-    --adapt_method prompt_w_mul_head \
-    --test_dataset svhn \
+    --adapt_method prompt_w_head \
     --epochs 50 \
     --lr 0.5 \
     --weight_decay 1e-4 \
-    --test_dataset svhn \
+    --test_dataset cifar100 \
     --num_gpus 8 \
     --num_workers 1 \
     --distance_threshold 20 \
